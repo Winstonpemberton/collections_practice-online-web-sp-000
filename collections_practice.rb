205cb2e7cf4cb31084dd52309fd083b7ac3b5104
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-12 22:45:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[0],array[1], array[2] = array[0],array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |strings|
+    strings.to_a
+    strings.each do |string|
+      string[2] = "$"
+    end
+    strings.join
+  end
+end
